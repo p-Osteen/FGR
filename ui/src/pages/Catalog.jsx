5be99bd39df9306584eae395b7d4bc1a18a9c3cb
Paul <paulopoyyayil@gmail.com>
2026-08-11 17:34:06 +0530

    Auto-update catalog

--- ui/src/pages/Catalog.jsx
+++ ui/src/pages/Catalog.jsx
@@ -23,7 +23,6 @@ const GameCard = React.memo(({ game }) => (
       <div className="game-info">
         <h3 className="game-title" title={game.title}>{game.title}</h3>
         <div className="game-meta">
-          {game.repackSize && <span className="repack-size">{game.repackSize}</span>}
           <span className="date">{formatDate(game.date)}</span>
         </div>
       </div>
